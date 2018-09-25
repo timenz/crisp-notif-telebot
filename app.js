a68e0bd148cf27e25897d86f3ffb75e256dd3abb
bang Tim <timenmail@gmail.com>
2018-09-26 05:48:42 +0700

    add command /piket, its for private usage

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var debug = require('debug')('app:app')
 const TelegramBot = require('node-telegram-bot-api');
+const request = require('request')
 
 const url = process.env.APP_URL || 'http://localhost:3000'
+const TOKEN = process.env.TELEGRAM_TOKEN || '';
+const sendTo = process.env.SEND_TO ? process.env.SEND_TO.split(',') : []
+const jadwalUrl = process.env.JADWAL_URL || null
 
 var indexRouter = require('./routes/index');
 
-const TOKEN = process.env.TELEGRAM_TOKEN || '';
-const sendTo = process.env.SEND_TO ? process.env.SEND_TO.split(',') : []
 
 const bot = new TelegramBot(TOKEN)
 
@@ -83,10 +85,58 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const getPiket = () => {
+  return new Promise((resolve, reject) => {
+    const dt = new Date()
+    if (jadwalUrl === null) {
+      debug('JADWAL_URL not set')
+      resolve([])
+      return
+    }
+    request({ url: jadwalUrl, json: true }, (err, res, body) => {
+      const piket = body[dt.getDate()] || []
+      resolve(piket)
+    })
+  })
+}
+
 bot.on('message', (msg) => {
   const id = msg.chat.id
-  debug('logged', id)
-  bot.sendMessage(id, 'thanks for messaging me');
+  const text = msg.text || null
+  let message = 'thanks for messaging me'
+  switch (text) {
+    case '/piket':
+      getPiket()
+        .then(result => {
+          message = `
+Yang piket hari ini
+
+-------------------------------------
+${result.join('\n')}
+-------------------------------------
+`
+          bot.sendMessage(id, message)
+        })
+        .catch(err => {
+          debug(err)
+        })
+      break
+    case '/about':
+      message = `
+About
+
+-------------------------------------
+author: bang tim
+url: https://www.timen.net
+repo: https://github.com/timenz/crisp-notif-telebot
+-------------------------------------
+`
+      bot.sendMessage(id, message)
+      break
+    default:
+      bot.sendMessage(id, message)
+      break
+  }
 });
 
 module.exports = app;
